e9643f7a9ead849099fb2aae128e846f7f8ba5f0
Wiklyn Craveiro <wiklynsc@gmail.com>
2025-04-04 13:49:03 -0300

    Adds comment related to the connection string

--- 04-api/back/database.py
+++ 04-api/back/database.py
@@ -3,6 +3,8 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 
 from settings import Settings
 
+# The database url comes from a .env file. It follows the structure:
+# 'postgresql+psycopg://user:pass@localhost:5432/db_name'
 engine = create_engine(Settings().DATABASE_URL)
 SessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
 Base = declarative_base()
